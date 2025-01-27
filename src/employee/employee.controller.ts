98230e71a802733735e158107268b5faf959049a
pratyusheducation82160 <pratyusheducation82160@gmail.com>
2025-01-27 20:39:36 +0530

    20:39-crud-ops

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get, Param } from "@nestjs/common";
 import { EmployeeService } from "./employee.service";
 
 @Controller("/employee")
 export class EmployeeController{
     constructor(private employeeService:EmployeeService){}
     @Get()
-    
     getAllEmployee(){
         return this.employeeService.getAllEmployee();
     }
-}
+
+    @Get(":id")
+    getEmployeeById(@Param("id") id:string){
+        return this.employeeService.getEmployeeById(parseInt(id));
+    }
+         
+    @Get("search/:email")
+    getEmployeeByEmail(@Param("email") email:string){
+    return this.employeeService.getEmployeeByEmail(email);
+    }
+    
+    @Delete(":id")
+    deleteEmployeeById(@Param("id") id:string){
+        return this.employeeService.deleteEmployeeById(parseInt(id));
+    }
+
+} 
--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -6,7 +6,19 @@ import { Repository } from "typeorm";
 @Injectable()
 export class EmployeeService{
     constructor(@InjectRepository(Employee)private employeeRepository:Repository<Employee>){}
-    getAllEmployee(){
-        return this.employeeRepository.find({});
+    async getAllEmployee(){
+        return await this.employeeRepository.find({});
     }
-}
+    async getEmployeeById(id:number){
+        return await this.employeeRepository.findOne({where:{id}});
+    }
+    async getEmployeeByEmail(email:string){
+        return await this.employeeRepository.findOne({where:{email}});
+    }
+
+    async deleteEmployeeById(id:number){
+        const emp=await this.getEmployeeById(id);
+        emp.statusDelete=true;
+        return this.employeeRepository.update(id,emp);
+    }
+    }
